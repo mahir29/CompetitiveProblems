e8d1c679a91b53c11a2067fabb32ebcc7e57f84c
Mahir Goel <mahirhardwar@gmail.com>
2021-05-23 17:41:11 +0530

    Quick Sort Algorithm

--- sorting/quickSort.cpp
+++ sorting/quickSort.cpp
@@ -1,72 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-// A utility function to swap two elements
-void swap(int* a, int* b)
+
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vp vector<pii>
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+
+int partition(vi &v, int s, int e)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
- 
-/* This function takes last element as pivot, places
-the pivot element at its correct position in sorted
-array, and places all smaller (smaller than pivot)
-to left of pivot and all greater elements to right
-of pivot */
-int partition (int arr[], int low, int high)
-{
-    int pivot = arr[high]; // pivot
-    int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
- 
-    for (int j = low; j <= high - 1; j++)
-    {
-        // If current element is smaller than the pivot
-        if (arr[j] < pivot)
-        {
-            i++; // increment index of smaller element
-            swap(&arr[i], &arr[j]);
+    int pivot=v[s];
+    int ptr=s+1;
+    for(int i=s+1;i<e;i++){
+        if(v[i]<v[s]){
+            swap(v[i],v[ptr]);
+            ptr++;
         }
+
+        swap(v[s],v[ptr-1]);
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+
+    return ptr-1;
 }
- 
-/* The main function that implements QuickSort
-arr[] --> Array to be sorted,
-low --> Starting index,
-high --> Ending index */
-void quickSort(int arr[], int low, int high)
+
+void quickSort(vi &v, int s, int e)
 {
-    if (low < high)
-    {
-        /* pi is partitioning index, arr[p] is now
-        at right place */
-        int pi = partition(arr, low, high);
- 
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
+    if (s >= e)
+        return;
+    int pivot=partition(v,s,e);
+    quickSort(v, s, pivot-1);
+    quickSort(v, pivot+1, e);
+   
 }
- 
-/* Function to print an array */
-void printArray(int arr[], int size)
+
+signed main()
 {
-    int i;
-    for (i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-}
- 
-// Driver Code
-int main()
-{
-    int arr[] = {10, 7, 8, 9, 1, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    quickSort(arr, 0, n - 1);
-    cout << "Sorted array: \n";
-    printArray(arr, n);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    vi v = {3, 2, 4, 8, 5, 9, 1};
+
+    quickSort(v, 0, v.size());
+
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+
+    //Write your code here
+
     return 0;
 }
