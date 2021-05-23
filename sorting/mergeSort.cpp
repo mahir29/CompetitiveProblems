e9ab16b5c21b6f1c498a102a5f95f6f70c67fa43
Mahir Goel <mahirhardwar@gmail.com>
2021-05-23 17:13:22 +0530

    Merge Sort Algorithm

--- sorting/mergeSort.cpp
+++ sorting/mergeSort.cpp
@@ -1,96 +1,62 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
- 
-// Merges two subarrays of arr[].
-// First subarray is arr[l..m]
-// Second subarray is arr[m+1..r]
-void merge(int arr[], int l, int m, int r)
-{
-    int n1 = m - l + 1;
-    int n2 = r - m;
- 
-    // Create temp arrays
-    int L[n1], R[n2];
- 
-    // Copy data to temp arrays L[] and R[]
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
- 
-    // Merge the temp arrays back into arr[l..r]
- 
-    // Initial index of first subarray
-    int i = 0;
- 
-    // Initial index of second subarray
-    int j = 0;
- 
-    // Initial index of merged subarray
-    int k = l;
- 
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
+
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vs vector<string>
+#define pii pair<int,int>
+#define vp vector<pii>
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(),v.end()
+
+void merge(vi &v,int s,int mid,int e){
+    int p=s;
+    int q=mid+1;
+
+    vi A;
+
+    int k=0;
+    
+    for(int i=s;i<=e;i++){
+        if(p>mid){
+            A[k++]=v[q++];
         }
-        else {
-            arr[k] = R[j];
-            j++;
+        else if(q>e){
+            A[k++]=v[p++];
         }
-        k++;
+        else if(v[p]<v[q]){
+            A[k++]=v[p++];
+        }
+        else{
+            A[k++]=v[q++];
+        }
     }
- 
-    // Copy the remaining elements of
-    // L[], if there are any
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
- 
-    // Copy the remaining elements of
-    // R[], if there are any
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+    
 }
- 
-// l is for left index and r is
-// right index of the sub-array
-// of arr to be sorted */
-void mergeSort(int arr[],int l,int r){
-    if(l>=r){
-        return;//returns recursively
-    }
-    int m =l+ (r-l)/2;
-    mergeSort(arr,l,m);
-    mergeSort(arr,m+1,r);
-    merge(arr,l,m,r);
+
+void mergeSort(vi &v,int s,int e){
+    if(s>=e)return;
+    int mid=(s+e)/2;
+    mergeSort(v,s,mid);
+    mergeSort(v,mid+1,e);
+    merge(v,s,mid,e);
 }
- 
-// UTILITY FUNCTIONS
-// Function to print an array
-void printArray(int A[], int size)
-{
-    for (int i = 0; i < size; i++)
-        cout << A[i] << " ";
-}
- 
-// Driver code
-int main()
-{
-    int arr[] = { 12, 11, 13, 5, 6, 7 };
-    int arr_size = sizeof(arr) / sizeof(arr[0]);
- 
-    cout << "Given array is \n";
-    printArray(arr, arr_size);
- 
-    mergeSort(arr, 0, arr_size - 1);
- 
-    cout << "\nSorted array is \n";
-    printArray(arr, arr_size);
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    vi v={3,2,4,8,5,9,1};
+
+    mergeSort(v,0,v.size());
+
+    for(int i=0;i<v.size();i++)
+    cout<<v[i]<<" ";
+
+    //Write your code here
+
     return 0;
 }
