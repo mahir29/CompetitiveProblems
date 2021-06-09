713238e0666bbaeefb014da3895d17158b63c03a
Mahir Goel <mahirhardwar@gmail.com>
2021-06-09 14:18:55 +0530

    Rotate Array

---
+++ lbQues/rotateArray.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vs vector<string>
+#define pii pair<int,int>
+#define vp vector<pii>
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(),v.end()
+#define mii map<int,int>
+#define w(x) int x; cin>>x; while(x--)
+
+void rotate(int arr[], int n)
+{
+    for(int i=n-2;i>=0;i--){
+        int temp=arr[i];
+        arr[i]=arr[i+1];
+        arr[i+1]=temp;
+    }
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    //Write your code here
+
+    return 0;
+}
