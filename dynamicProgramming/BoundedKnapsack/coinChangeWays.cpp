cb6c79c55550376a6161f4676902aacabc1fa80a
Mahir Goel <mahirhardwar@gmail.com>
2021-06-12 13:49:23 +0530

    Coin change ways

---
+++ dynamicProgramming/BoundedKnapsack/coinChangeWays.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vs vector<string>
+#define pii pair<int,int>
+#define vp vector<pii>
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(),v.end()
+#define mii map<int,int>
+#define w(x) int x; cin>>x; while(x--)
+
+
+int coinChange(int a[],int n,int sum){
+    int t[n+1][sum+1];
+
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<sum+1;j++){
+            if(i==0 && j==0)
+                t[i][j]=1;
+            else if(i==0)
+                t[i][j]=0;
+            else if(j==0)
+                t[i][j]=1;
+        }
+    }
+
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<sum+1;j++){
+            if(a[i-1]<=sum){
+                t[i][j]=t[i][j-a[i-1]]+t[i-1][j];
+            }
+            else{
+                t[i][j]=t[i-1][j];
+            }
+        }
+    }
+
+    return t[n][sum];
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    //Write your code here
+
+    return 0;
+}
--- dynamicProgramming/BoundedKnapsack/rodCutting.cpp
+++ dynamicProgramming/BoundedKnapsack/rodCutting.cpp
@@ -13,8 +13,11 @@ using namespace std;
 #define mii map<int,int>
 #define w(x) int x; cin>>x; while(x--)
 
-int rodCutting(int a[],int price[],int n,int sum){
-    int t[n+1][n+1];
+//l->length of rod
+//n->number of possible sizez of rod that we can cut
+
+int rodCutting(int a[],int price[],int n,int l){
+    int t[n+1][l+1];
 
     for(int i=0;i<n+1;i++){
         for(int j=0;j<n+1;j++){
@@ -28,8 +31,8 @@ int rodCutting(int a[],int price[],int n,int sum){
     }
 
     for(int i=1;i<n+1;i++){
-        for(int j=1;j<n+1;j++){
-            if(a[i-1]<=sum){
+        for(int j=1;j<l+1;j++){
+            if(a[i-1]<=l){
                 t[i][j]=max(price[i-1]+t[i][j-a[i-1]],t[i-1][j]);
             }
             else{
@@ -38,7 +41,7 @@ int rodCutting(int a[],int price[],int n,int sum){
         }
     }
 
-    return t[n][n];
+    return t[n][l];
 }
 
 signed main(){
