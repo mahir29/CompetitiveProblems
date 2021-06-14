bd732c9cc63f849b797f0c604ec336af368a20f3
Mahir Goel <mahirhardwar@gmail.com>
2021-06-14 13:00:45 +0530

    Longest Repeating Subsequence

---
+++ dynamicProgramming/longestCommonSubsequnce/longestRepeatingSub.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vs vector<string>
+#define pii pair<int,int>
+#define vp vector<pii>
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(),v.end()
+#define mii map<int,int>
+#define w(x) int x; cin>>x; while(x--)
+
+int subsequnce(string x,int m){
+    int t[m+1][m+1];
+
+    string y=x;
+
+    for(int i=0;i<m+1;i++){
+        for(int j=0;j<m+1;j++){
+            if(i==0||j==0){
+                t[i][j]=0;
+            }
+        }
+    }
+
+    for(int i=1;i<m+1;i++){
+        for(int j=1;j<m+1;j++){
+            if(x[i-1]==y[j-1] && i!=j){
+                t[i][j]=1+t[i-1][j-1];
+            }
+            else{
+                t[i][j]=max(t[i-1][j],t[i][j-1]);
+            }
+        }
+    }
+
+    return t[m][m];
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    //Write your code here
+
+    return 0;
+}
