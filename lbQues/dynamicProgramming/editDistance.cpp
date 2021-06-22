cedaa24ac9af3f7b8ff54fe12be0672b243dc172
Mahir Goel <mahirhardwar@gmail.com>
2021-06-22 13:43:04 +0530

    7.Edit Distance

---
+++ lbQues/dynamicProgramming/editDistance.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vp vector<pii>
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+#define mii map<int, int>
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+
+int editDistance(string s, string t)
+{
+    // Code here
+    int m = s.length();
+    int n = t.length();
+
+    int dp[m + 1][n + 1];
+
+    for (int i = 0; i < m + 1; i++)
+    {
+        dp[i][0] = i;
+    }
+
+    for (int i = 0; i < n + 1; i++)
+    {
+        dp[0][i] = i;
+    }
+
+    for (int i = 1; i < m + 1; i++)
+    {
+        for (int j = 1; j < n + 1; j++)
+        {
+            if (s[i - 1] == t[j - 1])
+            {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    //Write your code here
+
+    return 0;
+}
