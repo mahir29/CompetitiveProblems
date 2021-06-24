215a9f15c17e0b8688caf88751b5d50bae5cf484
Mahir Goel <mahirhardwar@gmail.com>
2021-06-24 11:20:50 +0530

    Reach score to the n

---
+++ lbQues/dynamicProgramming/31.CountSumGame.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ull unsigned long long
+#define vi vector<int>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vp vector<pii>
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+#define mii map<int, int>
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+
+long long int count(long long int n)
+{
+    long long int a[3];
+    a[0] = 3;
+    a[1] = 5;
+    a[2] = 10;
+
+    long long int t[4][n + 1];
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (long long int j = 0; j < n + 1; j++)
+        {
+            if (j == 0)
+            {
+                t[i][j] = 1;
+            }
+            else if (i == 0)
+            {
+                t[i][j] = 0;
+            }
+            else if (a[i - 1] <= j)
+            {
+                t[i][j] = t[i][j - a[i - 1]] + t[i - 1][j];
+            }
+            else
+            {
+                t[i][j] = t[i - 1][j];
+            }
+        }
+    }
+
+    return t[3][n];
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    //Write your code here
+
+    return 0;
+}
