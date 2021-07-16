2e488c170c2b148a9e0ba197325edeb2d2b73f48
Mahir Goel <mahirhardwar@gmail.com>
2021-07-16 12:06:41 +0530

    Min swaps

---
+++ hackerrank/minSwapsToSortArray.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vs vector<string>
+#define pii pair<int,int>
+#define vp vector<pii>
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(),v.end()
+#define mii map<int,int>
+#define w(x) int x; cin>>x; while(x--)
+
+int minSwaps(int arr[], int n)
+{
+    // Create an array of
+    // pairs where first
+    // element is array element
+    // and second element
+    // is position of first element
+    pair<int, int> arrPos[n];
+    for (int i = 0; i < n; i++)
+    {
+        arrPos[i].first = arr[i];
+        arrPos[i].second = i;
+    }
+ 
+    // Sort the array by array
+    // element values to
+    // get right position of
+    // every element as second
+    // element of pair.
+    sort(arrPos, arrPos + n);
+ 
+    // To keep track of visited elements.
+    // Initialize
+    // all elements as not visited or false.
+    vector<bool> vis(n, false);
+ 
+    // Initialize result
+    int ans = 0;
+ 
+    // Traverse array elements
+    for (int i = 0; i < n; i++)
+    {
+        // already swapped and corrected or
+        // already present at correct pos
+        if (vis[i] || arrPos[i].second == i)
+            continue;
+ 
+        // find out the number of  node in
+        // this cycle and add in ans
+        int cycle_size = 0;
+        int j = i;
+        while (!vis[j])
+        {
+            vis[j] = 1;
+ 
+            // move to next node
+            j = arrPos[j].second;
+            cycle_size++;
+        }
+ 
+        // Update answer by adding current cycle.
+        if (cycle_size > 0)
+        {
+            ans += (cycle_size - 1);
+        }
+    }
+ 
+    // Return result
+    return ans;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    //Write your code here
+
+    return 0;
+}
