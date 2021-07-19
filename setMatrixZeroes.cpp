81c737a8cb470d5e6965dd0688948a9457c2b280
Mahir Goel <mahirhardwar@gmail.com>
2021-07-19 18:43:14 +0530

    Set matrix zeroes

--- setMatrixZeroes.cpp
+++ setMatrixZeroes.cpp
@@ -1,44 +1,76 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define int long long
 #define ull unsigned long long
 #define vi vector<int>
 #define vs vector<string>
-#define pii pair<int,int>
+#define pii pair<int, int>
 #define vp vector<pii>
 #define pb push_back
 #define mp make_pair
-#define all(v) v.begin(),v.end()
-#define mii map<int,int>
-#define w(x) int x; cin>>x; while(x--)
+#define all(v) v.begin(), v.end()
+#define mii map<int, int>
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
 
-void solve(vector<vi> &mat){
-    int r=mat.size();
-    int c=mat[0].size();
-    bool col=true;
+void setZeroes(vector<vector<int>> &A)
+{
 
-    for(int i=0;i<r;i++){
-        if(mat[i][0]==0) col=false;
-        for(int j=1;j<c;j++){
-            if(mat[i][j]==0){
-                mat[i][0]=0;
-                mat[0][j]=0;
+    bool col = true, row = true;
+    int r = A.size();
+    int c = A[0].size();
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (i == 0 && A[i][j] == 0)
+                row = false;
+
+            if (j == 0 && A[i][j] == 0)
+                col = false;
+
+            if (A[i][j] == 0)
+            {
+                A[i][0] = 0;
+                A[0][j] = 0;
             }
         }
     }
 
-    for(int i=r-1;i>=0;i--){
-        for(int j=c-1;j>=0;j--){
-            if(mat[i][0]==0 || mat[0][j]==0) mat[i][j]=0;
+    for (int i = 1; i < r; i++)
+    {
+        for (int j = 1; j < c; j++)
+        {
+            if (A[i][0] == 0 || A[0][j] == 0)
+            {
+                A[i][j] = 0;
+            }
         }
-        if(col==false){
-             mat[i][0]=0;
+    }
+
+    if (row == false)
+    {
+        for (int i = 0; i < c; i++)
+        {
+            A[0][i] = 0;
+        }
+    }
+
+    if (col == false)
+    {
+        for (int i = 0; i < r; i++)
+        {
+            A[i][0] = 0;
         }
     }
 }
 
-signed main(){
+signed main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
